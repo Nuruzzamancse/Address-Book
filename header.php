6e6a8b8eb27696943ef2e40bcc144d9321965c59
Nuruzzamancse <mr.nuruzzamancse@gmail.com>
2018-02-17 14:58:53 +0600

    updated commit

---
+++ header.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="profile_pics/male.jpg" type="image/x-icon" />
+
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="title" content="test">
+
+    <title><?php echo 'Address Book'; ?></title>
+
+    <!-- Bootstrap -->
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="bootstrap/html5shiv.js"></script>
+    <script src="bootstrap/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="bootstrap/js/jquery-1.9.0.min.js"></script>
+</head>
+<body>
+<div class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="https://nuruzzamancse.github.io./" target="_blank"><span class="glyphicon glyphicon-home"></span> Nuruzzaman Khan</a>
+        </div>
+        <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1" >
+            <ul class="nav navbar-nav">
+                <li><a href="http://www.thesoftwareguy.in/php-tutorial/">PHP Tutorials</a></li>
+<!--                <li><a href="http://www.thesoftwareguy.in/working-demos/">More Demos</a></li>-->
+<!--                <li><a href="http://www.thesoftwareguy.in/mini-project/">More Projects</a></li>-->
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="container mainbody">
+    <div class="page-header">
+        <h1>Address Book</h1>
+    </div>
+    <div class="clearfix"></div>
